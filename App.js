44e2588018b4ffe0c90d38d2d02acbf7de38c3e5
Sandip Bhambre (Sandy) <sandeepbhambre@gmail.com>
2020-04-07 20:17:11 +0530

    Fix Drawer Issues

--- App.js
+++ App.js
@@ -1,54 +1,83 @@
-import * as React from 'react';
-import { Button, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createDrawerNavigator,DrawerActions } from '@react-navigation/drawer';
-import Donnor from './screens/Donnor'
-import Login from './screens/Login';
-import Signup from './screens/Signup';
-import Families from './screens/Families'
-import Icon from 'react-native-vector-icons'
+import * as React from "react";
+import { TouchableOpacity, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator, DrawerActions } from "@react-navigation/drawer";
+import Donnor from "./screens/Donnor";
+import Login from "./screens/Login";
+import Signup from "./screens/Signup";
+import Families from "./screens/Families";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
 const DonnorStack = createStackNavigator();
-const DonnorScreen=({navigation})=>(
- <DonnorStack.Navigator >
-   <DonnorStack.Screen name="Donnor List" component={Donnor}/>
- </DonnorStack.Navigator>
-  )
-  const FamiliesScreen=({navigation})=>(
-    <FamiliesStack.Navigator >
-      <FamiliesStack.Screen name="Families List" component={Families} options={{
-        headerLeft:()=>{
-          <Icon.Button name='ios-menu' size={25} onPress={()=>{
-           navigation.openDrawer()
-          }} />
-        }
-      }} />
-    </FamiliesStack.Navigator>
-     )
+const DonnorScreen = ({ navigation }) => (
+  <DonnorStack.Navigator>
+    <DonnorStack.Screen
+      options={{
+        headerLeft: () => (
+          <TouchableOpacity
+            style={styles.iconButton}
+            onPress={() => navigation.openDrawer()}
+            activeOpacity={1.0}
+          >
+            <Icon name="menu" size={24} color="#000" />
+          </TouchableOpacity>
+        ),
+      }}
+      name="Donnor List"
+      component={Donnor}
+    />
+  </DonnorStack.Navigator>
+);
+const FamiliesScreen = ({ navigation }) => (
+  <FamiliesStack.Navigator>
+    <FamiliesStack.Screen
+      name="Families List"
+      component={Families}
+      options={{
+        headerLeft: () => (
+          <TouchableOpacity
+            style={styles.iconButton}
+            onPress={() => navigation.openDrawer()}
+            activeOpacity={1.0}
+          >
+            <Icon name="menu" size={24} color="#000" />
+          </TouchableOpacity>
+        ),
+      }}
+    />
+  </FamiliesStack.Navigator>
+);
 function Roots() {
-  return(
-
-<Drawer.Navigator initialRouteName="Donnor">
-        <Drawer.Screen name="Donnor List" component={DonnorScreen} />
-        <Drawer.Screen name="Families List" component={FamiliesScreen}/>
-</Drawer.Navigator>
-
-  )
-  
+  return (
+    <Drawer.Navigator initialRouteName="Donnor">
+      <Drawer.Screen name="Donnor List" component={DonnorScreen} />
+      <Drawer.Screen name="Families List" component={FamiliesScreen} />
+    </Drawer.Navigator>
+  );
 }
 
 export default function App() {
- return (
+  return (
     <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Signup" component={Signup} />
-      
-      <Stack.Screen name="9ofa.tn" component={Roots} />
-    </Stack.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
+
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="9ofa.tn"
+          component={Roots}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  iconButton: {
+    padding: 16,
+  },
+});
